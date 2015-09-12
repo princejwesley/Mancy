a4a2ecdf8ba948f2de7f7e177096d13344484a62
Prince J Wesley <princejohnwesley@gmail.com>
2015-09-12 10:44:48 +0530

    Initial commit

---
+++ src/main/index.js
@@ -0,0 +1,41 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var menu = require('./menu');
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', onReady);
+app.on('activate-with-no-open-windows', onReady);
+
+function onReady() {
+
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 500,
+    resizable: true,
+    'web-preferences': {
+			'overlay-scrollbars': true
+		},
+    show: false,
+    title: 'Node REPL+'
+  });
+
+  mainWindow.loadUrl('file://' + __dirname + '/../../index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  menu.setMenu(app, mainWindow);
+}
---
+++ src/main/menu.js
@@ -0,0 +1,159 @@
+var Menu = require('menu');
+var MenuItem = require('menu-item');
+
+module.exports = {
+  setMenu: function(app, mainWindow) {
+    var template = [
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'CmdOrCtrl+Z',
+            role: 'undo'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+CmdOrCtrl+Z',
+            role: 'redo'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Cut',
+            accelerator: 'CmdOrCtrl+X',
+            role: 'cut'
+          },
+          {
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste'
+          },
+          {
+            label: 'Select All',
+            accelerator: 'CmdOrCtrl+A',
+            role: 'selectall'
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'CmdOrCtrl+R',
+            click: function(item, focusedWindow) {
+              if (focusedWindow)
+                focusedWindow.reload();
+            }
+          },
+          {
+            label: 'Toggle Full Screen',
+            accelerator: (function() {
+              if (process.platform === 'darwin')
+                return 'Ctrl+Command+F';
+              else
+                return 'F11';
+            })(),
+            click: function(item, focusedWindow) {
+              if (focusedWindow)
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+          },
+        ]
+      },
+      {
+        label: 'Window',
+        role: 'window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+          },
+          {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+          },
+        ]
+      },
+      {
+        label: 'Help',
+        role: 'help',
+        submenu: [
+          {
+            label: 'Learn More',
+            // TODO: add github project url
+            click: function() { require('shell').openExternal('http://electron.atom.io') }
+          },
+        ]
+      },
+    ];
+
+    if (process.platform === 'darwin') {
+      var name = app.getName();
+      template.unshift({
+        label: name,
+        submenu: [
+          {
+            label: 'About ' + name,
+            role: 'about'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Services',
+            role: 'services',
+            submenu: []
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Hide ' + name,
+            accelerator: 'Command+H',
+            role: 'hide'
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            role: 'hideothers'
+          },
+          {
+            label: 'Show All',
+            role: 'unhide'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          },
+        ]
+      });
+      // Window menu.
+      template[3].submenu.push(
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          role: 'front'
+        }
+      );
+    }
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  }
+};
