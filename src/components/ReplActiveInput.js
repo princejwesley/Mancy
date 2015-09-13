038d256c41b5fc60b9746d1c35c4127f28e19b3b
Prince J Wesley <princejohnwesley@gmail.com>
2015-09-14 04:36:18 +0530

    Tab focus & autocomplete

--- src/components/ReplActiveInput.js
+++ src/components/ReplActiveInput.js
@@ -3,15 +3,17 @@ import _ from 'lodash';
 import repl from 'repl';
 import {Readable, Writable} from 'stream';
 import {EOL} from 'os';
+import shell from 'shell';
 import ReplActions from '../actions/ReplActions';
 import ReplConstants from '../constants/ReplConstants';
 
 export default class ReplActiveInput extends React.Component {
   constructor(props) {
     super(props);
+    this.autoComplete = this.autoComplete.bind(this);
   }
   componentDidMount() {
-    this.initFocus();
+    this.focus();
     let cli = ReplActiveInput.getRepl();
     cli.output.write = this.addEntry.bind(this);
   }
@@ -21,12 +23,18 @@ export default class ReplActiveInput extends React.Component {
     cli.output.write = () => {};
   }
 
-  initFocus() {
-    // focus/selection
+  focus() {
+    // focus
     let node = React.findDOMNode(this);
     node.focus();
+
+    //set cursor at end
+    let range = document.createRange();
+    range.selectNodeContents(node);
+    range.collapse(false);
     let selection = window.getSelection();
-    selection.collapse(node, 0);
+    selection.removeAllRanges();
+    selection.addRange(range);
   }
 
   addEntry(buf) {
@@ -38,22 +46,44 @@ export default class ReplActiveInput extends React.Component {
     console.log(entry)
   }
 
-  onKeyDown(e) {
-    console.log(this)
-    let cli = ReplActiveInput.getRepl();
-    if(e.key === 'Tab') {
-      // cli.complete
-    } else if(e.key === 'Enter') {
+  autoComplete(__, completion) {
+    // console.log('auto complete', completion, this);
+    let [list, input] = completion;
+    if(list.length === 0) {
+      shell.beep();
+    } else if(list.length === 1) {
       const text = React.findDOMNode(this).innerText;
-      cli.input.emit('data', text);
-      cli.input.emit('data', EOL);
+      let lines = text.split(EOL);
+      let currentLine = lines.length - 1;
+      lines[currentLine] = lines[currentLine].replace(input, '') + list[0];
+      React.findDOMNode(this).innerText = lines.join(EOL);
     }
-    e.persist(); // remove after testing
-    console.log(e)
+    this.focus()
+  }
+
+  onKeyDown(e) {
+    let cli = ReplActiveInput.getRepl();
+    const text = React.findDOMNode(this).innerText.trim();
+
+    if(e.key === 'Tab') {
+      cli.complete(text, this.autoComplete.bind(this));
+      // avoid focus loss
+      e.preventDefault();
+    } else if(e.key === 'Enter') {
+      // emit last line
+      var lines = text.split(EOL);
+      var lastLine = lines[lines.length - 1];
+      cli.input.emit('data', lastLine);
+      cli.input.emit('data', EOL);
+    } else {
+      // try autoComplete and show results too
+    }
+    // e.persist(); // remove after testing
+    // console.log(e)
   }
   render() {
     return (
-      <div autoFocus className='repl-active-input' contentEditable={true} onKeyDown={this.onKeyDown.bind(this)}>
+      <div autoFocus className='repl-active-input' tabIndex="-1" contentEditable={true} onKeyDown={this.onKeyDown.bind(this)}>
       </div>
     );
   }
@@ -76,8 +106,8 @@ export default class ReplActiveInput extends React.Component {
       // writer: require('util').inspect,
       replMode: repl[ReplConstants.REPL_MODE],
     });
-    // console.log(nodeRepl)
 
+    // console.log(nodeRepl)
     // nodeRepl._domain.on('error', (err) => {
     //   console.log(err);
     // });
