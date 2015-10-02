562c9d581215292b391c2dd9e0762041b53e3c35
Prince J Wesley <princejohnwesley@gmail.com>
2015-10-02 14:34:57 +0530

    Mitigate node bug issues/3158

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReplSuggestions from './components/ReplSuggestions';
 import Repl from './components/Repl';
+import _ from 'lodash';
+
+
+(() => {
+  // Temporary fix for node bug : https://github.com/nodejs/node/issues/3158
+  let ownPropertyNames = Object.getOwnPropertyNames.bind(Object);
+
+  Object.getOwnPropertyNames = (o) => {
+    let result = ownPropertyNames(o);
+    let keys = Object.keys(o);
+    let difference = _.difference(keys, result);
+    return difference.length ? result.concat(difference) : result;
+  };
+})();
 
 // react entry point
-(function() {
+(() => {
   const repl = document.getElementById('node-repl-plus');
   React.render(<Repl />, repl);
   const suggestion = document.getElementById('node-repl-prompt-suggestions');
