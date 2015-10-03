2b0ffea62302b39c4a82d93b263badb5f9d8e688
Prince J Wesley <princejohnwesley@gmail.com>
2015-10-04 05:16:21 +0530

    Release gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,14 @@ import runSequence from 'run-sequence';
 import path from 'path';
 import _ from 'lodash';
 import mkdirp from 'mkdirp';
+import {argv} from 'yargs';
 import GitHubApi from 'github';
 import Electron from 'electron-packager';
 import Config from './package.json';
+import semver from 'semver';
+import ChildProcess from 'child_process';
+import {writeFileSync} from 'fs';
+import {basename, extname, dirname, join} from 'path';
 
 const $ = plugins();
 
@@ -31,7 +36,7 @@ const PATHS = {
   DIST: 'dist',
   TMP: 'tmp',
   ICON: 'icons'
-}
+};
 
 let onError = (err) => {
   $.util.beep();
@@ -53,20 +58,34 @@ async function mkdir(dir) {
   });
 }
 
-async function authenticate() {
-  let github = new GitHubApi({
-    version: "3.0.0",
-    debug: true
-  });
-
+async function authenticate(api) {
   github.authenticate({
     type: "oauth",
-    token: process.env.APP_TOKEN
+    token: process.env.GITHUB_TOKEN
   });
 }
 
-async function executable(platform = 'all', arch = 'all') {
-  return new Promise((resolve, reject) => {
+let zipExe = async (dists) => {
+  await mkdir(PATHS.TMP);
+  return Promise.all(_.map(dists, (dist) => {
+    let name = basename(dist);
+    return spawn('zip', [`${PATHS.TMP}/${name}.zip`, '-r', `${PATHS.DIST}/${name}`]);
+  }));
+}
+
+let spawn = async (command, args, options) => {
+  let cb = (resolve, reject) => {
+    let exe = ChildProcess.spawn(command, args, options);
+    exe.stdout.on('data', (data) => onError(data.toString('utf8')));
+    exe.stderr.on('data', (err) => onError(err.toString('utf8')));
+    exe.on('close', resolve);
+    exe.on('error', reject);
+  };
+  return new Promise(cb);
+}
+
+let executable = async (platform = 'all', arch = 'all') => {
+  let cb = (resolve, reject) => {
     Electron({
       name: `${_.capitalize(Config.name)}`,
       platform,
@@ -88,8 +107,74 @@ async function executable(platform = 'all', arch = 'all') {
       }
       resolve(result);
     });
-  });
-}
+  };
+  return new Promise(cb);
+};
+
+let releaseCommit = async () => {
+  let {v} = argv;
+  if(['major', 'minor', 'patch'].indexOf(v) === -1) {
+    v = 'patch';
+  }
+  Config.version = semver.inc(Config.version, v);
+  let newConfig = JSON.stringify(Config, null, 2);
+
+  // update package.json
+  writeFileSync('package.json', newConfig);
+
+  await spawn('git', ['commit', '-m', `Release v${Config.version}`, 'package.json']);
+  await spawn('git', ['push']);
+};
+
+let createRelease = async (api) => {
+  let {url} = Config.repository;
+  let owner = basename(dirname(url));
+  let repo = basename(url, extname(url));
+
+  let cb = (resolve, reject) => {
+    api.releases.createRelease({
+      owner: owner,
+      repo: repo,
+      tag_name: `v${Config.version}`,
+      body: argv.desc || '',
+      name: argv.name || `v${Config.version}`,
+      draft: argv.draft === 'yes'
+    }, (err, result) => {
+      if(err) {
+        onError(err);
+        return reject(err);
+      }
+      resolve(result);
+    });
+  };
+  return new Promise(cb);
+};
+
+let uploadAsset = async (api, id, dists) => {
+  let {url} = Config.repository;
+  let owner = basename(dirname(url));
+  let repo = basename(url, extname(url));
+
+  return Promise.all(_.map(dists, (dist) => {
+    let name = `${basename(dist)}.zip`;
+    let filePath = join(PATHS.TMP, name);
+    return new Promise((resolve, reject) => {
+      api.releases.uploadAsset({
+        owner: owner,
+        repo: repo,
+        id: id,
+        name: name,
+        filePath: filePath
+      }, (err, result) => {
+        if(err) {
+          onError(err);
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }));
+};
 
 const options = {
   react: {
@@ -175,11 +260,26 @@ gulp.task('packageAll', ['build'], (cb) => {
 });
 
 
-gulp.task('release',['build', 'packageAll'], (cb) => {
+gulp.task('release',['build'], (cb) => {
   (async () => {
     try {
-      await authenticate();
-      //TODO: create version commit, create release and upload assets
+      let github = new GitHubApi({
+        version: "3.0.0",
+        debug: true
+      });
+
+      await authenticate(github);
+      await releaseCommit();
+
+      gulp.src(['package.json'], { base: '.' })
+        .pipe(gulp.dest(PATHS.APP));
+
+      let dists = await executable();
+
+      await zipExe(dists);
+      let {id} = await createRelease(github);
+      await uploadAsset(github, id, dists);
+
       cb();
     } catch(err) {
       onError(err);
