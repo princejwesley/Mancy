5ff5e2162f5619e4fcc3de8f6c004710aaa9226d
David Street <david@exclamationlabs.com>
2016-02-18 12:31:14 -0500

    Fix typo in zoom options array

--- src/components/ReplPageZoom.js
+++ src/components/ReplPageZoom.js
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 import ReplPreferencesActions from '../actions/ReplPreferencesActions';
 
-const zoomOptions = [0.75, 0.9, 1, 1.1, 1.2, 1.25, 1.3, 1.4, 1.5, 1.6, 1.7, 1.75, 1.8, 1,9, 2, 2.1, 2.2, 2.25, 2.3, 2.4, 2.5];
+const zoomOptions = [0.75, 0.9, 1, 1.1, 1.2, 1.25, 1.3, 1.4, 1.5, 1.6, 1.7, 1.75, 1.8, 1.9, 2, 2.1, 2.2, 2.25, 2.3, 2.4, 2.5];
 export default class ReplPageZoom extends React.Component {
   constructor(props) {
     super(props);
