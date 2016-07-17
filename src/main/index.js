d39ca6f9ffcd557096c7cb811f1d9112c2082abc
Prince J Wesley <princejohnwesley@gmail.com>
2016-07-17 09:56:46 +0530

    Predicate for Linux non Unity cases

--- src/main/index.js
+++ src/main/index.js
@@ -211,7 +211,7 @@ app.on('browser-window-focus', (event, window) => {
   window.$focus = true;
   dockNotificationCache[window.id] = 0;
   if (process.platform === 'darwin' ||
-      (process.platform === 'linux' &&
+      (process.platform === 'linux' && app.isUnityRunning &&
         app.isUnityRunning())) {
     app.setBadgeCount(0);
   }
@@ -392,7 +392,7 @@ ipcMain.on('application:download', function(event, buffer) {
 ipcMain.on('application:dock-message-notification', function(event, id) {
   dockNotificationCache[id] = dockNotificationCache[id] + 1;
   if (process.platform === 'darwin' ||
-      (process.platform === 'linux' &&
+      (process.platform === 'linux' && app.isUnityRunning &&
         app.isUnityRunning())) {
     app.setBadgeCount(dockNotificationCache[id]);
   }
